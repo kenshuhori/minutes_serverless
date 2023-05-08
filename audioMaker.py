e0456e2cf9ba5f51fb682717ec686375712b2aca
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-05-08 22:10:43 +0900

    yt-dlpを利用した音声ファイルのs3アップロード (#1)
    * youtubeから音声取得
    
    * fix
    
    * fix
    
    * fix
    
    * Create README.md
    
    * use yt-dlp
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- audioMaker.py
+++ audioMaker.py
@@ -1,16 +1,35 @@
 import json
+import boto3
+import subprocess, base64, os
+import yt_dlp
 
+BUCKET_NAME='minutes-prod-contents'
+
+s3 = boto3.resource('s3')
+ydl_opts = {
+  'format': 'm4a',
+  'outtmpl': '/tmp/videos/%(id)s.%(ext)s'
+}
 
 def execute(event, context):
-    video_id = event['queryStringParameters']['video_id']
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+  video_id = 'YcPwmCj0iGI'
+  part = 0
+  returncode, error_message, file = extract(video_id, part)
+  if returncode != 1:
+    upload(file)
+  else:
+    print(error_message)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+def extract(video_id, part):
+  urls = [f'https://www.youtube.com/watch?v={video_id}']
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    error_code = ydl.download(urls)
+  return error_code, 'error_message if exists...', f'/tmp/videos/{video_id}.m4a'
 
-    return response
+def upload(file):
+  response = s3.Object(BUCKET_NAME, os.path.basename(file)).upload_file(file)
+  return response
+
+if __name__ == "__main__":
+  event = { 'data': '', 'part': '0' }
+  execute(event, '')
