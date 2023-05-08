f0395eb3dfcb1de8b3f85e6e3ed943218f3f29e8
KenshuHori <kenshuhori@gmail.com>
2023-05-08 15:55:47 +0900

    use yt-dlp

--- audioMaker.py
+++ audioMaker.py
@@ -1,13 +1,18 @@
 import json
 import boto3
 import subprocess, base64
+import yt_dlp
 
 BUCKET_NAME='minutes-prod-contents'
 
 s3 = boto3.resource('s3')
+ydl_opts = {
+  'format': 'm4a',
+  'outtmpl': '/tmp/videos/%(id)s.%(ext)s'
+}
 
 def execute(event, context):
-  video_id = "YcPwmCj0iGI"
+  video_id = 'YcPwmCj0iGI'
   part = 0
   returncode, error_message, file = extract(video_id, part)
   if returncode != 1:
@@ -16,11 +21,12 @@ def execute(event, context):
     print(error_message)
 
 def extract(video_id, part):
-  result = subprocess.run(["yt-dlp", "-f", "m4a", "-o", f"/tmp/videos/%(channel_id)s/{part}/%(id)s.%(ext)s", video_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-  return result.returncode, result.stderr, filename(result.stdout)
+  urls = [f'https://www.youtube.com/watch?v={video_id}']
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    error_code = ydl.download(urls)
+  return error_code, 'error_message if exists...', open(f'/tmp/videos/{video_id}.m4a', 'r')
 
 def upload(file):
-  # filename = file.replace('/tmp/videos/', '')
   response = s3.upload_file(file, BUCKET_NAME)
   return response
 
