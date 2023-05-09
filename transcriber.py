43b118468e17e0b229163fef228931ceef5a4b40
KenshuHori <kenshuhori@gmail.com>
2023-05-09 17:00:27 +0900

    fix

--- transcriber.py
+++ transcriber.py
@@ -1,5 +1,16 @@
+import os
+import openai
+
 def main(event, context):
   print('🔥🔥🔥')
+  fname = "YcPwmCj0iGI.m4a"
+  openai.api_key = os.environ['OPENAI_API_KEY']
+  audio_file = open(fname, "rb")
+  transcript = openai.Audio.transcribe("whisper-1", audio_file)
+  txt = transcript['text']
+  f = open(fname + ".txt", "w")
+  f.write(txt)
+  f.close()
 
 if __name__ == "__main__":
   event = { 'data': '', 'part': '0' }
