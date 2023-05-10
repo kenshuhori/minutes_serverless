9e3aebe3a59e7e1f0fe5480a960457bda627482e
KenshuHori <kenshuhori@gmail.com>
2023-05-10 20:36:42 +0900

    スペースで改行する

--- transcriber.py
+++ transcriber.py
@@ -7,11 +7,11 @@ def main(event, context):
   openai.api_key = os.environ['OPENAI_API_KEY']
   audio_file = open(fname, "rb")
   transcript = openai.Audio.transcribe("whisper-1", audio_file)
-  txt = transcript['text']
+  txt = "\n".join(transcript['text'].split())
   f = open(fname + ".txt", "w")
   f.write(txt)
   f.close()
 
 if __name__ == "__main__":
   event = { 'data': '', 'part': '0' }
-  main(event, '')
+  hoge(event, '')
