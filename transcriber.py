7f689d2da46a31d1faf85dadb07063243645e8a0
Kenshu Hori <37034086+kenshuhori@users.noreply.github.com>
2023-05-15 18:30:36 +0900

    パラメータストアの値を取得する (#4)
    * パラメータストアの値を取得する
    
    * Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -10,35 +10,40 @@ BUCKET_NAME='minutes-prod-contents'
 s3 = boto3.resource('s3')
 
 def main(event, context):
-    print('🔥🔥🔥')
     print(event)
-    get_parameter()
+    fname = "leothefootball"
+    print("🔥")
+    openai.api_key = get_parameter()
     event_object = event['Records'][0]['s3']['object']['key']
     bucket = s3.Bucket(BUCKET_NAME)
+    print("🔥🔥")
     obj = bucket.Object(event_object).get()
-
-def hoge(event, context):
-    print('🔥')
-    fname = "YcPwmCj0iGI.m4a"
-    openai.api_key = os.environ['OPENAI_API_KEY']
-    audio_file = open(fname, "rb")
+    print("🔥🔥🔥")
+    audio_file = open(obj, "rb")
+    print("🔥🔥🔥🔥")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print("🔥🔥🔥🔥🔥")
     txt = "\n".join(transcript['text'].split())
     f = open(fname + ".txt", "w")
     f.write(txt)
     f.close()
+    upload(fname + ".txt")
 
 def get_parameter():
-    print('🔥🔥🔥🔥')
     end_point = 'http://localhost:2773'
-    path = '/systemsmanager/parameters/get/?name=/minutes/prod/OPENAI_API_KEY'
-    url = end_point + path
+    path = '/systemsmanager/parameters/get/'
+    query_parameter = '?name=/minutes/prod/OPENAI_API_KEY&withDecryption=true'
+    url = end_point + path + query_parameter
     headers = {
         'X-Aws-Parameters-Secrets-Token': os.environ['AWS_SESSION_TOKEN']
     }
     res = requests.get(url, headers=headers)
-    print(res)
+    return res.json()['Parameter']['Value']
+
+def upload(file):
+    response = s3.Object(BUCKET_NAME, f'/documents/{os.path.basename(file)}').upload_file(file)
+    return response
 
 if __name__ == "__main__":
   event = { 'data': '', 'part': '0' }
-  hoge(event, '')
+  main(event, '')
